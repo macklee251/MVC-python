b5a1b3b9b602bbb6f4ebf45daa09fd257482aea3
mack lee <114943402+macklee251@users.noreply.github.com>
2024-03-07 21:05:09 -0300

    .

--- model.py
+++ model.py
@@ -4,9 +4,6 @@ class Pessoa():
         self.nome = nome
         self.idade = idade
         self.cpf = cpf
-
-    def valida_cpf(self) -> bool:
-        return True if len(self.cpf) == 11 else False
         
     def __str__(self) -> str:
         return f'Nome: {self.nome}, Idade: {self.idade}, CPF: {self.cpf}'
